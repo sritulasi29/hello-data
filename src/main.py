367077dbe3d94cd0ff70867274b97b7677f64c17
sri_tulasi29 <saminenisritulasi777@gmail.com>
2025-12-19 11:07:54 +0530

    Read CSV and generate data summary

--- src/main.py
+++ src/main.py
@@ -1,1 +1,15 @@
-print("Hello, Data Engineering World!")
+import pandas as pd 
+df=pd.read_csv("C:/Users/prasa/hello-data/data/students.csv")
+num_rows=df.shape[0]
+num_cols=df.shape[1]
+columns=list(df.columns)
+summary=f"""
+STUDENT DATA INFO
+-----------------
+Number of rows: {num_rows}
+Number of columns: {num_cols}
+columns: {columns}
+"""
+with open("C:/Users/prasa/hello-data/output/summary.txt","w") as f:
+    f.write(summary)
+print(summary)
